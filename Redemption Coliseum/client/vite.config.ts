69d747943bc2bb4d5fae178c46972b3452b99be5
Reth <Rene.Thol@gmx.de>
2026-07-10 10:50:05 +0200

    First installable PWA version (alpha or early beta)

--- Redemption Coliseum/client/vite.config.ts
+++ Redemption Coliseum/client/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   server: {
@@ -9,4 +10,35 @@ export default defineConfig({
     outDir: "dist",
     emptyOutDir: true,
   },
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Redemption Coliseum",
+        short_name: "Redemption",
+        id: "/",
+        description: "Redemption TCG Coliseum",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        navigateFallback: "/index.html",
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
+      },
+    }),
+  ],
 });
